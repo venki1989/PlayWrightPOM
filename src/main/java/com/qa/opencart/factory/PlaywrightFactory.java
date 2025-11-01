5e8b2fb0e8dd313c6494834c33c6d56a84992bc0
Venkatesh M <venkatesh.m@metricstream.com>
2025-11-01 14:07:07 +0530

    factory

--- src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
+++ src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -63,7 +63,7 @@ public class PlaywrightFactory {
 			break;
 		case "chrome":
 			tlBrowser.set(
-					getPlaywright().chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(true)));
+					getPlaywright().chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(false)));
 			break;
 		case "edge":
 			tlBrowser.set(
